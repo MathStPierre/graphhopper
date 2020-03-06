8b339a4a784aa42292a551669e44729c2349ebd6
Mathieu St-Pierre <mathieu.st-pierre@curbside.com>
2020-03-06 03:41:08 -0500

    Add js node assets and use small ipe pbf by default.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,8 @@ scripts/container/create-cache
 java $JAVA_OPTS \
   -Ddw.server.applicationConnectors[0].bindHost=0.0.0.0 \
   -Ddw.server.applicationConnectors[0].port=80 \
-  -Dgraphhopper.graph.location=graph-cache \
-  -Dgraphhopper.datareader.file=kanto.osm.pbf \
-  -Dgraphhopper.gtfs.file=rail.zip \
-  -jar target/graphhopper-1.0-SNAPSHOT.jar \
+  -Ddw.graphhopper.graph.location=graph-cache \
+  -Ddw.graphhopper.datareader.file=prince-edward-island-latest.osm.pbf \
+  -jar web/target/graphhopper-web-1.0-SNAPSHOT.jar \
   server \
-  resources/config.yml
+  scripts/container/config.yml
